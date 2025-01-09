9c9d01622073a70ff05096bc29f32566297e5ae2
tkz420 <58402790+tkz420@users.noreply.github.com>
2025-01-09 02:31:12 +0100

    #

--- src/components/MediaItem.jsx
+++ src/components/MediaItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
     import './MediaItem.css';
+    import supabase from '../utils/supabaseClient';
 
     function MediaItem({ item }) {
+      const storageUrl = supabase.storage.from('media').getPublicUrl(item.thumb_path).data.publicUrl;
       return (
         <div className="media-item">
-          {item.thumb_path && <img src={item.thumb_path} alt={item.url} />}
+          {item.thumb_path && <img src={storageUrl} alt={item.url} />}
         </div>
       );
     }
