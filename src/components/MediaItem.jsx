ee517f7d6a1733157cf3504e4c02e10402d685dc
tkz420 <58402790+tkz420@users.noreply.github.com>
2025-01-09 02:29:04 +0100

    thumbnails

--- src/components/MediaItem.jsx
+++ src/components/MediaItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './MediaItem.css';
+    import './MediaItem.css';
 
-function MediaItem({ item }) {
-  return (
-    <div className="media-item">
-      {item.url && <img src={item.url} alt={item.url} />}
-    </div>
-  );
-}
+    function MediaItem({ item }) {
+      return (
+        <div className="media-item">
+          {item.thumb_path && <img src={item.thumb_path} alt={item.url} />}
+        </div>
+      );
+    }
 
-export default MediaItem;
+    export default MediaItem;
